f659f3b40f70ee9daed415972ce65f84e19e5f11
Balkrishna <boli@unifiedcommunications.com>
2023-12-08 16:12:07 +0545

    added code to retrieve url and export them in Excel file

--- testCases/test_stackoverflow.py
+++ testCases/test_stackoverflow.py
@@ -1,7 +1,12 @@
+from selenium.webdriver.common.by import By
+
 from utilities.readProperties import ReadConfig
 from pageObjects.stackoverflow import StackOverflow
 from utilities.customLogger import LogGen
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
+import pandas as pd
 
 
 class Test_stackoverflow:
@@ -22,5 +27,24 @@ class Test_stackoverflow:
         self.so.setPassword(self.password)
         assert self.driver.title == "Log In - Stack Overflow"
         self.so.clickLogin()
-        time.sleep(10)
+        time.sleep(5)
         assert self.driver.title == "Stack Overflow - Where Developers Learn, Share, & Build Careers"
+
+        self.so.searchQuery("python automation")
+        time.sleep(5)
+
+        # Retrieve the URLs of the top 10 search results
+        result_urls = []
+        links = self.driver.find_elements(By.TAG_NAME, "a")
+        for link in links[:10]:
+            href = link.get_attribute("href")
+            if href and "stackoverflow.com" in href:
+                result_urls.append(href)
+        print(result_urls)
+
+        # Close the browser
+        self.driver.quit()
+
+        # Export URLs to an Excel file
+        df = pd.DataFrame(result_urls, columns=["URLs"])
+        df.to_excel(".\\Reports\\"+"stack_overflow_results.xlsx", index=False)
